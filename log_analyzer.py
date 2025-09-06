bbbe5d4e5c574b9f54fcedc54b7c11cec5bd4f70
JeneviveRiyaDsilva <jenevivecks221@gmail.com>
2025-09-06 18:34:23 +0530

    Added SUCCESS vs FAILED login visualization

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,8 +1,7 @@
-# Project 2: Network Log Analyzer with Success & Failed Visualization
+
 
 import matplotlib.pyplot as plt
 
-# Step 1: Read the log file
 log_file = "sample_logs.txt"
 
 logs = []
@@ -10,7 +9,6 @@ with open(log_file, "r") as file:
     for line in file:
         logs.append(line.strip())
 
-# Step 2: Count failed and successful logins per IP
 failed_logins = {}
 success_logins = {}
 
@@ -23,10 +21,9 @@ for log in logs:
     elif "Login: SUCCESS" in log:
         success_logins[ip_address] = success_logins.get(ip_address, 0) + 1
 
-# Step 3: Detect suspicious IPs (more than 2 failed attempts)
 suspicious_ips = [ip for ip, count in failed_logins.items() if count > 2]
 
-# Step 4: Print results
+
 print("=== Failed Login Attempts ===")
 for ip, count in failed_logins.items():
     print(f"{ip}: {count} failed attempts")
@@ -38,7 +35,7 @@ if suspicious_ips:
 else:
     print("No suspicious activity detected.")
 
-# Step 5: Visualization
+
 all_ips = list(set(list(failed_logins.keys()) + list(success_logins.keys())))
 failed_counts = [failed_logins.get(ip, 0) for ip in all_ips]
 success_counts = [success_logins.get(ip, 0) for ip in all_ips]
